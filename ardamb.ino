d2397c28d75b2c09835684070dd4c4fa497a6dd7
deivi <dpsrz22@gmail.com>
2017-07-28 22:28:53 +0200

    Nuevo sensor de luz y DHT22 Se ha optimizado el SD y escribe en formato CSV

--- ardamb.ino
+++ ardamb.ino
@@ -6,6 +6,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <SD.h>
+#include <BH1750.h>
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -17,91 +18,100 @@ const int DHTPin = 2;
 DHT dht(DHTPin, DHTTYPE);
 
 float temp, humedad;
-int ldr_pin = 0;
-int ldr_val = 0;
 int suelo = 0;
 
 const int chipSelect = 3;
 File myFile; 
 
 Adafruit_BMP280 bme;
-
+BH1750 Bh;
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
   bme.begin(0x76);
+  Bh.begin(BH1750_CONTINUOUS_HIGH_RES_MODE);
 
   if(!SD.begin(chipSelect)){
     Serial.println("No hay MicroSD insertado");
     return;
     }
+
+  File myFile = SD.open("datos.txt",FILE_WRITE); //Abrimos o creamos el archivo llamado datos.txt
+
+  if (myFile.size()<=0)
+  {
+    myFile.print("Tªbar (ºC);");
+    myFile.print("Presión (Pa);");
+    myFile.print("Altitud (m);");
+    myFile.print("Humedad (%);");
+    myFile.print("Luminosidad (lux);");
+    myFile.print("Hª suelo (%);");
+    myFile.println("TªDHT (ºC);");
+    myFile.flush();
   }
+  myFile.close(); 
+}
 
 void loop() {
+  uint16_t lux = Bh.readLightLevel(); 
   humedad = dht.readHumidity();
   temp = dht.readTemperature();
   suelo = analogRead(1);
   float presion = bme.readPressure();
   float altitud = bme.readAltitude(1013.25);
   float temperatura = bme.readTemperature();
-  File myFile = SD.open("datos.txt",FILE_WRITE);
 
   if(isnan(humedad) || isnan(temp)) {
     Serial.println("Fallo de lectura del sensor DHT");
     return;
   }
 
-  Serial.print("Humedad: ");
-  Serial.print(humedad);
-  Serial.print("%");
-  ldr_val = map(analogRead(ldr_pin),0,1023,100,0);
-  Serial.print(" Luz: ");
-  Serial.print(ldr_val);
-  Serial.print("%");
+  File myFile = SD.open("datos.txt",FILE_WRITE); //Abrimos o creamos el archivo llamado datos.txt
+  
   Serial.print(" Humedad suelo: ");
   Serial.print(suelo);
 
-  if(suelo <= 300) { Serial.print(" ENCHARCADO"); }
+  if(suelo <= 300) { Serial.print(" ENCHARCADO "); }
   if((suelo > 300) and (suelo < 700)) { Serial.print(" HUMEDAD "); }
   if(suelo >= 700) { Serial.print(" SECO "); }
 
-  Serial.print(" Temperatura: ");
+  Serial.print(" Tªbar: ");
   Serial.print(temperatura);
-  Serial.print(" ºC");
+  Serial.print(" ºC ");
+  Serial.print(" TªDHT: ");
+  Serial.print(temp);
+  Serial.print(" ºC ");
   Serial.print(" Presión: ");
   Serial.print(presion);
   Serial.print(" Pa");
   Serial.print(" Altitud: ");
   Serial.print(altitud);
-  Serial.println(" m");
+  Serial.print(" m ");
+  Serial.print(" Humedad (%): ");
+  Serial.print(humedad);
+  Serial.print(" Luminosidad (lx): ");
+  Serial.println(lux);
 
-  myFile.print("Humedad (%): ");
-  myFile.print(humedad);
-  myFile.print(";");
 
-  myFile.print("Luz (%): ");
-  myFile.print(ldr_val);
-  myFile.print(";");
-
-  myFile.print("Humedad del suelo (%): ");
-  myFile.print(suelo);
-  myFile.print(";");
-
-  myFile.print("Temperatura (ºC): ");
+  //Escribimos datos en fichero
   myFile.print(temperatura);
   myFile.print(";");
-
-  myFile.print("Presión (Pa): ");
   myFile.print(presion);
   myFile.print(";");
-
-  myFile.print("Altitud (m): ");
   myFile.print(altitud);
+  myFile.print(";");
+  myFile.print(humedad);
+  myFile.print(";");
+  myFile.print(lux);
+  myFile.print(";");
+  myFile.print(suelo);
+  myFile.print(";");
+  myFile.print(temp);
   myFile.println(";");
 
+  myFile.flush();
   myFile.close();
-
   delay(10000);
 
 }
